f1e5844ae551460a3d737cff2697a348f60a3bdb
Akshay-Kumar1 <akshayk24396@gmail.com>
2018-08-22 15:59:11 +0530

    Branch

---
+++ simple2.java
@@ -0,0 +1,14 @@
+class Demo2{
+	
+	public void sum(int a , int b){
+	int sum = a+b;
+	System.out.println("Sum is "+sum);
+}
+
+	public static void main(String[] args){
+
+	Demo2 d2 = new Demo2();
+	d2.sum(10,20);
+}
+
+}
